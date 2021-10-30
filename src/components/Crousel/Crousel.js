b6a176c495f4919bce8062ef44042e1c85a0f266
Anusha <anushakancheti202@gmail.com>
2021-10-30 18:15:06 +0530

    commit

---
+++ src/components/Crousel/Crousel.js
@@ -0,0 +1,34 @@
+import "./Crousel.css";
+
+import { Carousel } from "react-bootstrap";
+import data from '../../data';
+import React from "react";
+
+function CrouselView() {
+  return (
+    <div>
+    <div className="container-fluid crousel-wrapper">
+     <Carousel>
+       {
+         data["carousel-images"].map(item=>{
+           return(  <Carousel.Item>
+           <img
+             className="d-block w-100"
+             src={item}
+             alt="First slide"
+             width="100%"
+             
+           />
+         
+            </Carousel.Item>
+           )
+         })
+       }
+ 
+  </Carousel>
+    </div>
+    </div>
+  );
+}
+
+export default CrouselView;
